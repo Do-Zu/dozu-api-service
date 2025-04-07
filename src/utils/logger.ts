3f5315d88722e47a0d7dbc44b4a24a5a1af12cb2
= <=cinepa.org@gmail.com>
2025-04-07 23:01:51 +0700

    feat(core): update log for metadata

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,7 @@
 import { createLogger, format, transports } from 'winston';
 import { config } from '@/config/env.config';
 
-// Define log levels and colors
+//  log levels and colors
 const levels = {
   error: 0,
   warn: 1,
@@ -10,12 +10,10 @@ const levels = {
   debug: 4,
 };
 
-// Determine level based on environment
 const level = () => {
-  return config.env === 'production' ? 'error' : 'debug';
+  return config?.env === 'production' ? 'error' : 'debug';
 };
 
-// Define colors for each level
 const colors = {
   error: 'red',
   warn: 'yellow',
@@ -24,32 +22,28 @@ const colors = {
   debug: 'blue',
 };
 
-// Add colors to winston
 format.colorize().addColors(colors);
 
-// Define the format for logs
 const logFormat = format.combine(
   format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
   format.colorize({ all: true }),
-  format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  format.printf(info => {
+    const metadata = info.metadata ? ` ${JSON.stringify(info.metadata)}` : '';
+    return `${info.timestamp} ${info.level}: ${info.message}${metadata}`;
+  })
 );
 
-// Define transports for logs
 const logTransports = [
-  // Console transport for all logs
   new transports.Console(),
 
-  // File transport for errors
   new transports.File({
     filename: 'logs/error.log',
     level: 'error',
   }),
 
-  // File transport for all logs
   new transports.File({ filename: 'logs/all.log' }),
 ];
 
-// Create the logger
 const logger = createLogger({
   level: level(),
   levels,
