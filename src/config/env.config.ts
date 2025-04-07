1919b431c8ce29c6be8e233cb1b2c026039f71bb
= <=cinepa.org@gmail.com>
2025-04-07 17:51:24 +0700

    feat(core): rm redundant code

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,14 +2,11 @@ import dotenv from 'dotenv';
 import path from 'path';
 import * as fs from 'fs';
 
-// Correct way to get directory path in CommonJS
 const rootPath = path.resolve(__dirname, '../../');
 
-// Ensure the environment file exists
 const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
 const envPath = path.resolve(rootPath, envFile);
 
-// Check if env file exists, if not use default values
 if (fs.existsSync(envPath)) {
   dotenv.config({
     path: envPath,
