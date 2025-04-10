ba25b9fc80152512cd35fdc3d1c7104d85ff8e40
= <=cinepa.org@gmail.com>
2025-04-10 17:57:36 +0700

    feat(core); update setting drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,26 @@
-
-import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+import path from 'path';
+import fs from 'fs';
+
+const env = process.env.NODE_ENV || 'development';
+const envPath = path.resolve(process.cwd(), `.env${env !== 'development' ? '.' + env : ''}`);
+
+if (fs.existsSync(envPath)) {
+  dotenv.config({ path: envPath });
+} else {
+  dotenv.config();
+}
+
+const name_db = 'dozu';
+const databaseUrl =
+  process.env.DATABASE_URL || `postgres://postgres:123456@localhost:5432/${name_db}`;
+
 export default defineConfig({
   out: './drizzle',
-  schema: './dist/models',
+  schema: './src/models',
   dialect: 'postgresql',
   dbCredentials: {
-    // url: process.env.DATABASE_URL!,
-    url: 'postgres://postgres:123456@localhost:5432/postgres',//todo:Actually use env
+    url: databaseUrl,
   },
 });
