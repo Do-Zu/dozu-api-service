0096e7925f9ba54ce81cd5158ec8d1fe9496ffe2
= <=cinepa.org@gmail.com>
2025-05-05 16:21:24 +0700

    fix(model):[production] fix for build stage on production

--- src/models/reminder.model.ts
+++ src/models/reminder.model.ts
@@ -1,7 +1,7 @@
 import { pgTable, serial, integer, varchar, timestamp, boolean, pgEnum } from 'drizzle-orm/pg-core';
 import { usersTable } from '@/models/user.model';
 import { schedulesTable } from '@/models/schedule.model';
-import { studySlotsTable } from '@/models/studySlot.model';
+import { studySlotTable } from '@/models/studySlot.model';
 
 // Create enum for reminder status
 export const reminderStatusEnum = pgEnum('reminder_status', ['pending', 'accepted', 'declined']);
@@ -9,7 +9,7 @@ export const reminderStatusEnum = pgEnum('reminder_status', ['pending', 'accepte
 export const remindersTable = pgTable('reminders', {
   id: serial('id').primaryKey(),
   userId: integer('user_id').references(() => usersTable.userId, { onDelete: 'set null' }),
-  taskId: integer('task_id').references(() => studySlotsTable.id, { onDelete: 'set null' }),
+  taskId: integer('task_id').references(() => studySlotTable.id, { onDelete: 'set null' }),
   originalScheduleId: integer('original_schedule_id').references(() => schedulesTable.id, {
     onDelete: 'set null',
   }),
