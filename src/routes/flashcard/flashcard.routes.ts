0b68a78ddde0dca460591ff6b0c8865d4bd4f7b1
KyAnh1126 <anh261104@gmail.com>
2025-05-16 15:21:57 +0700

    update flashcard routes using flashcard controller class

--- src/routes/flashcard/flashcard.routes.ts
+++ src/routes/flashcard/flashcard.routes.ts
@@ -1,13 +1,12 @@
 import { globalAsyncHandler } from "@/middleware/handler/handler.v2";
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import { registerRoute } from "@/routes/register.routes"; 
-import flashcardController, { getDate, updateDate } from "@/controllers/flashcard/flashcard.controller";
+import FlashcardController from "@/controllers/flashcard/flashcard.controller";
 import { validateUser } from "@/middleware/validations/validator";
 import { validateFlashcardsAdded, validateFlashcardsBatch, validateTopicId } from "@/middleware/validations/flashcard.validation";
-import flashcardRepo from "@/repositories/flashcard.repo";
-import logger from "@/utils/logger";
 
 const router = Router();
+const flashcardController = new FlashcardController();
 
 globalAsyncHandler(router);
 
@@ -22,22 +21,12 @@ router.get('/test', validateUser(), (req, res) => {
     res.json({ message: 'Success' });
 })
 
-router.get('/test-date', getDate);
-router.put('/test-date', updateDate);
+// router.get('/test-date', getDate);
+// router.put('/test-date', updateDate);
 router.put('/:flashcardId/track', flashcardController.handleTrackSingleFlashcard);
 
-router.get('/practice', flashcardController.handleGetFlashcardsPracticed);
+router.get('/practice', flashcardController.handleGetFlashcardsPracticedForUser);
 router.put('/:flashcardId/put-to-practice', flashcardController.handlePutFlashcardToPractice);
-
-router.put('/test-update', async(req, res, next) => {
-    const flashcard = { flashcardId: 22, front: 'Pen', back: 'Cây bút' };
-    try {
-        const flashcardUpdated = await flashcardRepo.handleUpdateSingleFlashcardForTopic(flashcard);
-        logger.info(flashcardUpdated);
-    } catch(err) {
-        next(err);
-    }
-})
 
 registerRoute('/flashcards', router, {
     description: 'Flashcards API for CRUD single flashcard',
