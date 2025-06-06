3349ea97fcab84bfdd598e9a45a3daa366b8793f
= <=cinepa.org@gmail.com>
2025-06-06 23:03:32 +0700

    feat(schedule)[v1.0]: implement generate schedule for user

--- src/controllers/schedule/schedule.controller.ts
+++ src/controllers/schedule/schedule.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { scheduleService } from '@/services/schedule/schedule.service';
 import { SuccessResponse } from '@/core/success';
+import { BadRequest } from '@/core/error';
 
 /**
  * Controller class for Schedule functionality
@@ -23,7 +24,25 @@ class ScheduleController {
    * @param res - Express response object
    */
   async generateSchedule(req: Request, res: Response) {
-    const data = await scheduleService.generateSchedule();
+    const { userId, fromDate, toDate, timezone } = req.body;
+
+    if (!userId) {
+      throw new BadRequest('user is required');
+    }
+
+    if (!fromDate || !toDate) {
+      throw new BadRequest('fromDate and toDate are required');
+    }
+
+    if (new Date(fromDate) > new Date(toDate)) {
+      throw new BadRequest('fromDate must be before toDate');
+    }
+
+    if (!timezone) {
+      throw new BadRequest('timezone is required');
+    }
+
+    const data = await scheduleService.generateSchedule({ userId, fromDate, toDate, timezone });
 
     SuccessResponse.ok(res, data);
   }
