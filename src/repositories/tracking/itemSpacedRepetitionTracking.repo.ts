176e2633f45286ad2b951ad74a4a5abd0fe8efcb
KyAnh1126 <anh261104@gmail.com>
2025-06-09 08:29:54 +0700

    add topicId to handleInsertDefaultFlashcardSpacedRepetitions function

--- src/repositories/tracking/itemSpacedRepetitionTracking.repo.ts
+++ src/repositories/tracking/itemSpacedRepetitionTracking.repo.ts
@@ -4,13 +4,15 @@ import { itemSpacedRepetitionTrackingTable } from '@/models';
 export default class ItemSpacedRepetitionTrackingRepo {
   public async handleInsertDefaultFlashcardSpacedRepetitions(
     userId: number,
+    topicId: number,
     flashcardIds: number[]
   ) {
     const values = flashcardIds.map(flashcardId => ({
       userId,
+      topicId,
       itemId: flashcardId,
       type: 'flashcard',
-    })) as { userId: number; itemId: number; type: 'flashcard' }[];
+    })) as { userId: number; topicId: number, itemId: number; type: 'flashcard' }[];
     await db.insert(itemSpacedRepetitionTrackingTable).values(values);
   }
 }
