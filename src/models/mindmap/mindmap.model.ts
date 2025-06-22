b548f29530da04b02a8b08c5a6ec274fbe45ca13
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2025-06-23 00:21:46 +0700

    chore(mindmap):make isRoot optional to prevent error

--- src/models/mindmap/mindmap.model.ts
+++ src/models/mindmap/mindmap.model.ts
@@ -1,37 +1,37 @@
-import { pgTable,  varchar, jsonb, timestamp, serial, integer } from 'drizzle-orm/pg-core';
+import { pgTable, varchar, jsonb, timestamp, serial, integer } from 'drizzle-orm/pg-core';
 
 import { topicsTable } from '../topic.model';
 
 type NodeData = {
-  id: string;
-  position: { x: number; y: number };
-  data: {
-    label: string;
-    isRoot: boolean;
-    pageStartIndex?: number;
-    pageEndIndex?: number;
-  };
+    id: string;
+    position: { x: number; y: number };
+    data: {
+        label: string;
+        isRoot?: boolean;
+        pageStartIndex?: number;
+        pageEndIndex?: number;
+    };
 };
 
 type EdgeData = {
-  id: string;
-  source: string;
-  target: string;
+    id: string;
+    source: string;
+    target: string;
 };
 
 export type MindmapData = {
-  nodes: NodeData[];
-  edges: EdgeData[];
+    nodes: NodeData[];
+    edges: EdgeData[];
 };
 
 export const mindmapsTable = pgTable('mindmaps', {
-  mindmapId: serial('mindmap_id').primaryKey(),
-  topicId: integer('topic_id')
-    .notNull()
-    .references(() => topicsTable.topicId, { onDelete: 'cascade' }),
-  title: varchar('title', { length: 255 }).default('New mind map'),
-  mindmapData: jsonb('mindmap_data').$type<MindmapData>(),
-  createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+    mindmapId: serial('mindmap_id').primaryKey(),
+    topicId: integer('topic_id')
+        .notNull()
+        .references(() => topicsTable.topicId, { onDelete: 'cascade' }),
+    title: varchar('title', { length: 255 }).default('New mind map'),
+    mindmapData: jsonb('mindmap_data').$type<MindmapData>(),
+    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
 });
 
 export type SelectMindmap = typeof mindmapsTable.$inferSelect;
