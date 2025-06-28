8b37495d28409b70066c7a88d8adefad54847bcd
KyAnh1126 <anh261104@gmail.com>
2025-06-28 16:08:00 +0700

    feat(topic-flashcard-v1.2): prettier, update endpoint url from practice to learning

--- src/routes/flashcard/flashcard.routes.ts
+++ src/routes/flashcard/flashcard.routes.ts
@@ -2,10 +2,7 @@ import { globalAsyncHandler } from '@/middleware/handler/handler.v2';
 import { Router } from 'express';
 import { registerRoute } from '@/routes/register.routes';
 import FlashcardController from '@/controllers/flashcard/flashcard.controller';
-import {
-  validateFlashcardsBatch,
-  validateTopicId,
-} from '@/middleware/validations/flashcard.validation';
+import { validateFlashcardsBatch, validateTopicId } from '@/middleware/validations/flashcard.validation';
 import { authMiddleware } from '@/middleware/auth.middleware';
 
 const router = Router();
@@ -16,23 +13,19 @@ globalAsyncHandler(router);
 router.use(authMiddleware);
 router.get('/', validateTopicId(), flashcardController.handleGetAllFlashcardsForTopic);
 
-router.post(
-  '/batch',
-  validateTopicId(),
-  validateFlashcardsBatch(),
-  flashcardController.handleBatchFlashcardsForTopic
-);
+router.post('/batch', validateTopicId(), validateFlashcardsBatch(), flashcardController.handleBatchFlashcardsForTopic);
 
 router.put('/:flashcardId/track', flashcardController.handleTrackSingleFlashcard);
 
 // todo: đổi url
-router.get('/practice', flashcardController.handleGetFlashcardsLearningForUser);
+router.get('/learning', flashcardController.handleGetFlashcardsLearningForUser);
+router.get('/learning/:topicId', flashcardController.handleGetFlashcardsLearningForTopic);
 router.put('/:flashcardId/put-to-learning', flashcardController.handlePutFlashcardToLearning);
 
 registerRoute('/flashcards', router, {
-  description: 'Flashcards API for CRUD single flashcard',
-  version: 'v1',
-  isEnabled: true,
+    description: 'Flashcards API for CRUD single flashcard',
+    version: 'v1',
+    isEnabled: true,
 });
 
 export const flashcardRoutes = router;
