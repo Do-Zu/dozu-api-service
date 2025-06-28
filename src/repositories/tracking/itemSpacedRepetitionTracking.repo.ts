c29b4e3054aac48f744601666d81c6ec4ab80f46
KyAnh1126 <anh261104@gmail.com>
2025-06-28 16:07:23 +0700

    feat(topic-flashcard-v1.2): prettier

--- src/repositories/tracking/itemSpacedRepetitionTracking.repo.ts
+++ src/repositories/tracking/itemSpacedRepetitionTracking.repo.ts
@@ -2,17 +2,17 @@ import db from '@/libs/drizzleClient.lib';
 import { itemSpacedRepetitionTrackingTable } from '@/models';
 
 export default class ItemSpacedRepetitionTrackingRepo {
-  public async handleInsertDefaultFlashcardSpacedRepetitions(
-    userId: number,
-    topicId: number,
-    flashcardIds: number[]
-  ) {
-    const values = flashcardIds.map(flashcardId => ({
-      userId,
-      topicId,
-      itemId: flashcardId,
-      type: 'flashcard',
-    })) as { userId: number; topicId: number, itemId: number; type: 'flashcard' }[];
-    await db.insert(itemSpacedRepetitionTrackingTable).values(values);
-  }
+    public async handleInsertDefaultFlashcardSpacedRepetitions(
+        userId: number,
+        topicId: number,
+        flashcardIds: number[]
+    ) {
+        const values = flashcardIds.map(flashcardId => ({
+            userId,
+            topicId,
+            itemId: flashcardId,
+            type: 'flashcard',
+        })) as { userId: number; topicId: number; itemId: number; type: 'flashcard' }[];
+        await db.insert(itemSpacedRepetitionTrackingTable).values(values);
+    }
 }
