905c1a28e1d165c048fea798baffd0139961651c
KyAnh1126 <anh261104@gmail.com>
2025-06-28 16:07:11 +0700

    feat(topic-flashcard-v1.2): prettier, update query to get number of flashcards due today

--- src/repositories/topic.repo.ts
+++ src/repositories/topic.repo.ts
@@ -1,79 +1,68 @@
 import db from '@/libs/drizzleClient.lib';
-import { flashcardsTable, topicsTable } from '@/models';
+import { flashcardsTable, itemSpacedRepetitionTrackingTable, topicsTable } from '@/models';
 import { ITopicBasic, ITopicAdded, ITopicUpdated } from '@/types/topic/topic.type';
-import { count, eq } from 'drizzle-orm';
+import { format } from 'date-fns';
+import { count, eq, sql } from 'drizzle-orm';
 
-export type ITopicForUser = ITopicBasic & { flashcardsCount?: number };
+export type ITopicForUser = ITopicBasic & { flashcardsCount?: number, flashcardsDueToday?: number };
 export type ITopicsForUserReturned = ITopicForUser[];
 
 class TopicRepo {
-  public async handleGetSingleTopic(topicId: number): Promise<ITopicBasic | undefined> {
-    const topic = await db.query.topicsTable.findFirst({
-      where: eq(topicsTable.topicId, topicId),
-      columns: {
-        topicId: true,
-        name: true,
-        description: true,
-      },
-    });
-    return topic;
-  }
-
-  public async handleGetAllTopicsForUser(userId: number): Promise<ITopicsForUserReturned> {
-    let topics: ITopicForUser[] = await db
-      .select({
-        topicId: topicsTable.topicId,
-        name: topicsTable.name,
-        description: topicsTable.description,
-      })
-      .from(topicsTable)
-      .where(eq(topicsTable.userId, userId));
-
-    for (let i = 0; i < topics.length; ++i) {
-      let topic = topics[i] as ITopicForUser;
-      const result = await db
-        .select({
-          flashcardsCount: count(),
-        })
-        .from(topicsTable)
-        .innerJoin(flashcardsTable, eq(flashcardsTable.topicId, topicsTable.topicId))
-        .where(eq(topicsTable.topicId, topic.topicId));
-
-      const { flashcardsCount }: { flashcardsCount: number } = result[0];
-
-      topic['flashcardsCount'] = flashcardsCount;
-      topics[i] = topic;
+    public async handleGetSingleTopic(topicId: number): Promise<ITopicBasic | undefined> {
+        const topic = await db.query.topicsTable.findFirst({
+            where: eq(topicsTable.topicId, topicId),
+            columns: {
+                topicId: true,
+                name: true,
+                description: true,
+            },
+        });
+        return topic;
     }
 
-    return topics;
-  }
+    public async handleGetAllTopicsForUser(userId: number): Promise<ITopicsForUserReturned> {
+        const today = format(new Date(), 'yyyy-MM-dd');
+        let topics: ITopicForUser[] = await db
+            .select({
+                topicId: topicsTable.topicId,
+                name: topicsTable.name,
+                description: topicsTable.description,
+                imageUrl: topicsTable.imageUrl,      
+                flashcardsCount: count(),     
+                flashcardsDueToday: sql<number>`CAST(COUNT(CASE WHEN item_spaced_repetition_tracking.next_review <= ${today} THEN 1 END) AS INT)`.as('flashcardsDueToday')
+            })
+            .from(topicsTable)
+            .innerJoin(flashcardsTable, eq(flashcardsTable.topicId, topicsTable.topicId))
+            .innerJoin(itemSpacedRepetitionTrackingTable, eq(itemSpacedRepetitionTrackingTable.itemId, flashcardsTable.flashcardId))
+            .where(eq(topicsTable.userId, userId))
+            .groupBy(topicsTable.topicId)
 
-  public async handleInsertSingleTopicForUser(topic: ITopicAdded): Promise<ITopicForUser> {
-    const result = await db
-      .insert(topicsTable)
-      .values(topic)
-      .returning({ 
-        topicId: topicsTable.topicId,
-        name: topicsTable.name,
-        description: topicsTable.description,
-      });
-    
-    let ret = result[0] as ITopicForUser;
-    return ret;
-  }
+        return topics;
+    }
 
-  public async handleUpdateSingleTopic(topicId: number, topic: ITopicUpdated): Promise<ITopicForUser> {
-    const [ret] = await db.update(topicsTable).set(topic).where(eq(topicsTable.topicId, topicId)).returning({
-      topicId: topicsTable.topicId,
-      name: topicsTable.name,
-      description: topicsTable.description,    
-    });
-    return ret;
-  }
+    public async handleInsertSingleTopicForUser(topic: ITopicAdded): Promise<ITopicForUser> {
+        const result = await db.insert(topicsTable).values(topic).returning({
+            topicId: topicsTable.topicId,
+            name: topicsTable.name,
+            description: topicsTable.description,
+        });
 
-  public async handleDeleteSingleTopic(topicId: number): Promise<void> {
-    await db.delete(topicsTable).where(eq(topicsTable.topicId, topicId));
-  }
+        let ret = result[0] as ITopicForUser;
+        return ret;
+    }
+
+    public async handleUpdateSingleTopic(topicId: number, topic: ITopicUpdated): Promise<ITopicForUser> {
+        const [ret] = await db.update(topicsTable).set(topic).where(eq(topicsTable.topicId, topicId)).returning({
+            topicId: topicsTable.topicId,
+            name: topicsTable.name,
+            description: topicsTable.description,
+        });
+        return ret;
+    }
+
+    public async handleDeleteSingleTopic(topicId: number): Promise<void> {
+        await db.delete(topicsTable).where(eq(topicsTable.topicId, topicId));
+    }
 }
 
 export default TopicRepo;
