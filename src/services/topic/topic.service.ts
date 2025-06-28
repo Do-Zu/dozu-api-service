fdae0da0450459ba28685e1955560e04aace47f0
KyAnh1126 <anh261104@gmail.com>
2025-06-28 16:08:48 +0700

    feat(topic-flashcard-v1.2): prettier

--- src/services/topic/topic.service.ts
+++ src/services/topic/topic.service.ts
@@ -4,41 +4,34 @@ import { ITopicBasic, ITopicAdded, ITopicUpdated } from '@/types/topic/topic.typ
 const topicRepo = new TopicRepo();
 
 class TopicService {
-  public async handleIsExistedTopic(topicId: number): Promise<boolean> {
-    const topic = await topicRepo.handleGetSingleTopic(topicId);
-    return topic ? true : false;
-  }
+    public async handleIsExistedTopic(topicId: number): Promise<boolean> {
+        const topic = await topicRepo.handleGetSingleTopic(topicId);
+        return topic ? true : false;
+    }
 
-  public async handleGetSingleTopic(
-    topicId: number
-  ): Promise<ITopicBasic | undefined> {
-    const topic = await topicRepo.handleGetSingleTopic(topicId);
-    return topic;
-  }
+    public async handleGetSingleTopic(topicId: number): Promise<ITopicBasic | undefined> {
+        const topic = await topicRepo.handleGetSingleTopic(topicId);
+        return topic;
+    }
 
-  public async handleGetAllTopicsForUser(userId: number): Promise<ITopicsForUserReturned> {
-    const topics = await topicRepo.handleGetAllTopicsForUser(userId);
-    return topics;
-  }
+    public async handleGetAllTopicsForUser(userId: number): Promise<ITopicsForUserReturned> {
+        const topics = await topicRepo.handleGetAllTopicsForUser(userId);
+        return topics;
+    }
 
-  public async handleInsertSingleTopicForUser(
-    topic: ITopicAdded
-  ): Promise<ITopicForUser> {
-    const ret = await topicRepo.handleInsertSingleTopicForUser(topic);
-    return ret;
-  }
+    public async handleInsertSingleTopicForUser(topic: ITopicAdded): Promise<ITopicForUser> {
+        const ret = await topicRepo.handleInsertSingleTopicForUser(topic);
+        return ret;
+    }
 
-  public async handleUpdateSingleTopic(
-    topicId: number,
-    topic: ITopicUpdated
-  ): Promise<ITopicForUser> {
-    const ret = await topicRepo.handleUpdateSingleTopic(topicId, topic);
-    return ret;
-  }
+    public async handleUpdateSingleTopic(topicId: number, topic: ITopicUpdated): Promise<ITopicForUser> {
+        const ret = await topicRepo.handleUpdateSingleTopic(topicId, topic);
+        return ret;
+    }
 
-  public async handleDeleteSingleTopic(topicId: number): Promise<void> {
-    await topicRepo.handleDeleteSingleTopic(topicId);
-  }
+    public async handleDeleteSingleTopic(topicId: number): Promise<void> {
+        await topicRepo.handleDeleteSingleTopic(topicId);
+    }
 }
 
 export default TopicService;
