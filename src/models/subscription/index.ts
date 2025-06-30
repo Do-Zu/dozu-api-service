17f3cc0fc53b8ab1a94a9a13ff1e3ed826311107
= <=cinepa.org@gmail.com>
2025-06-30 20:24:18 +0700

    chore(models): remove incorrect path import

--- src/models/subscription/index.ts
+++ src/models/subscription/index.ts
@@ -3,5 +3,3 @@ export * from './feature.model';
 export * from './planFeature.model';
 export * from './userSubscription.model';
 export * from './userFeatureUsage.model';
-export * from './billingHistory.model';
-export * from './coupon.model';
