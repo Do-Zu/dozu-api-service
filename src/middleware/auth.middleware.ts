5727171a0af1b38e4fdd6179ab31a7672fb16532
KyAnh1126 <anh261104@gmail.com>
2025-07-05 18:37:33 +0700

    feat(class): add validateTeacher middleware function

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -4,33 +4,45 @@ import { NextFunction, Request, Response } from 'express';
 
 import jwt from 'jsonwebtoken';
 import logger from '@/utils/logger';
-import { BadRequest } from '@/core/error';
-import { sanitizeUserObject } from '@/utils/auth/authHelpers.utils';
+import { BadRequest, Forbidden } from '@/core/error';
+import { getUserRolesFromRequest, sanitizeUserObject } from '@/utils/auth/authHelpers.utils';
 
 const SECRET = process.env.JWT_SECRET; // make sure to use env vars in production
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers['authorization'];
-  const accessToken = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+    const authHeader = req.headers['authorization'];
+    const accessToken = authHeader && authHeader.split(' ')[1]; // Bearer <token>
 
-  if (!accessToken) {
-    throw new BadRequest('Access token is required');
-  }
-  if (!SECRET) {
-    throw new Error('JWT_SECRET is not defined in environment variables');
-  }
+    if (!accessToken) {
+        throw new BadRequest('Access token is required');
+    }
+    if (!SECRET) {
+        throw new Error('JWT_SECRET is not defined in environment variables');
+    }
 
-  try {
-    const decoded: any = jwt.verify(accessToken, SECRET);
-    //.verify Validates expiration by default
-    // const sanitizedUser = sanitizeUserObject(decoded.user);
+    try {
+        const decoded: any = jwt.verify(accessToken, SECRET);
+        //.verify Validates expiration by default
+        // const sanitizedUser = sanitizeUserObject(decoded.user);
 
-    req.currentUser = decoded; // add `user` to Request via type augmentation
+        req.currentUser = decoded; // add `user` to Request via type augmentation
 
-    next();
-  } catch (error) {
-    console.log(error);
-    logger.warn('Invalid token');
-    throw new BadRequest('Unauthorized: Invalid token');
-  }
+        next();
+    } catch (error) {
+        console.log(error);
+        logger.warn('Invalid token');
+        throw new BadRequest('Unauthorized: Invalid token');
+    }
+};
+
+export const validateTeacher = async (req: Request, res: Response, next: NextFunction) => {
+    const roles = await getUserRolesFromRequest(req);
+    const isTeacher = roles.find((role) => role.name === 'teacher');
+    if(isTeacher) {
+        next();
+    } else {
+        const message = 'Forbidden: Require teacher to access the resources';
+        logger.warn(message);
+        throw new Forbidden(message);
+    }
 };
