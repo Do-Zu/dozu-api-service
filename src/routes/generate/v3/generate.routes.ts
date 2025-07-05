8a213fe83e66ae6c9cd740fb66b569a5b64bfce0
= <=cinepa.org@gmail.com>
2025-07-05 15:28:34 +0700

    feat(subscription): apply middleware check  usage for generate endpoint

--- src/routes/generate/v3/generate.routes.ts
+++ src/routes/generate/v3/generate.routes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 import { generateController } from '@/controllers/generate/v3/generate.controller';
 import { globalAsyncHandler } from '@/middleware/handler/handler.v2';
 import { registerRoute } from '../../register.routes';
+import subscriptionMiddleware from '@/middleware/subscription/subscript.middleware';
+import { authMiddleware } from '@/middleware/auth.middleware';
 
 const router = Router();
 
 globalAsyncHandler(router);
 
-router.post('/text/llm', generateController.generateContent);
+router.use(authMiddleware);
+
+router.post('/text/llm', subscriptionMiddleware.handleSubscription, generateController.generateContent);
 
 router.post('/text/llm/status', generateController.getGenerateContentStatus);
 
