c752e0a09ce6e603b17aa40fc96b5ced92b03f59
KyAnh1126 <anh261104@gmail.com>
2025-07-05 18:47:06 +0700

    feat(class): add classId field

--- src/models/topic.model.ts
+++ src/models/topic.model.ts
@@ -1,8 +1,12 @@
 import { pgTable, serial, integer, varchar, text, timestamp, vector } from 'drizzle-orm/pg-core';
 import { usersTable } from '@/models/user.model'; // adjust path as needed
+import { classesTable } from '@/models/class-based-learning/class.model';
 
 export const topicsTable = pgTable('topics', {
     topicId: serial('topic_id').primaryKey(),
+    // topic belong to which class, classId can be NULL if that topic belong to a user (student) with no class-based learning
+    classId: integer('class_id')
+        .references(() => classesTable.classId, { onDelete: 'cascade' }),
     userId: integer('user_id')
         .notNull()
         .references(() => usersTable.userId, { onDelete: 'cascade' }),
