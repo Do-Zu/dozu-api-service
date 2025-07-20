fa855f032b6c38d0ea315c1c50a2fca2f4809777
thai-hoang-03 <buithaihoang03@gmail.com>
2025-07-21 03:34:58 +0700

    chore(question-quiz): additional middleware for question routes

--- src/routes/question/question.routes.ts
+++ src/routes/question/question.routes.ts
@@ -4,10 +4,13 @@ import { registerRoute } from '@/routes/register.routes';
 import { questionController } from '@/controllers/question/question.controller';
 import { validateTopicId } from '@/middleware/validations/flashcard.validation';
 import { validateBatchQuestions } from '@/middleware/validations/question.validation';
+import { authMiddleware } from '@/middleware/auth.middleware';
 
 
 const router = Router();
 globalAsyncHandler(router);
+
+router.use(authMiddleware);
 
 router.post('/batch', validateTopicId(), validateBatchQuestions() ,questionController.handleBatchQuestionsForTopic);
 
