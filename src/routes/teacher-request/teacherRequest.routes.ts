4b1ca894f92dc3f090462931da82edd3709cd53f
KyAnh1126 <anh261104@gmail.com>
2025-08-12 13:42:36 +0700

    feat(refactor/kyanh): refactor code related to teacher request feature

--- src/routes/teacher-request/teacherRequest.routes.ts
+++ src/routes/teacher-request/teacherRequest.routes.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from '@/middleware/auth.middleware';
 import { globalAsyncHandler } from '@/middleware/handler/handler.v2';
 import { Router } from 'express';
 import { registerRoute } from '../register.routes';
-import { validateIdParam } from '@/middleware/validations/params.validation';
+import paramsValidator from '@/core/validations/params.validator';
 
 const router = Router();
 globalAsyncHandler(router);
@@ -14,8 +14,8 @@ router.get('/', teacherRequestController.getRequest);
 router.post('/', teacherRequestController.sendRequest);
 router.get('/all', teacherRequestController.getAllRequests);
 router.get('/pending', teacherRequestController.getPendingRequests);
-router.patch('/:requestId/approve', validateIdParam('requestId'), teacherRequestController.approveRequest);
-router.patch('/:requestId/reject', validateIdParam('requestId'), teacherRequestController.rejectRequest);
+router.patch('/:requestId/approve', paramsValidator.validateId('requestId'), teacherRequestController.approveRequest);
+router.patch('/:requestId/reject', paramsValidator.validateId('requestId'), teacherRequestController.rejectRequest);
 
 registerRoute('/teacher-requests', router, {
     description: 'Teacher Requests API endpoints',
