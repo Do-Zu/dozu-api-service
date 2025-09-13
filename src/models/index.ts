1f5a4b2fb438d11ad84521abb3828d0947707c35
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2025-09-13 14:39:17 +0700

    fix(class-feed): added classFeed.model to src/models/index.ts to allow using drizzle

--- src/models/index.ts
+++ src/models/index.ts
@@ -60,3 +60,6 @@ export * from '@/models/class-based-learning/classEnrollment.model';
 export * from '@/models/class-based-learning/classTopicComment.model';
 
 export * from '@/models/teacher-request/teacherRequest.model';
+
+export * from '@/models/class-based-learning/classFeed.model';
+
