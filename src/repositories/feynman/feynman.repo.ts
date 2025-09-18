ae3758109861e9c398e057f72b197e70c91e3515
vinhpm <phanminhvinh3010023@gmail.com>
2025-09-18 11:57:32 +0700

    feat(feynman): initial service

---
+++ src/repositories/feynman/feynman.repo.ts
@@ -0,0 +1,11 @@
+
+import db from '@/libs/drizzleClient.lib';
+
+/**
+ * Repository for Feynman data access operations
+ */
+class FeynmanRepository {
+  
+}
+
+export const feynmanRepo = new FeynmanRepository();
