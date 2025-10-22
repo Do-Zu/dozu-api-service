81cf0814d812c88b7bb228388bb7b4d2b17a57ec
vinhpm <phanminhvinh3010023@gmail.com>
2025-10-22 19:24:38 +0700

    feat(package): initial for db design

---
+++ src/models/package/package.model.ts
@@ -0,0 +1,24 @@
+import { pgTable, serial, integer, varchar, timestamp, foreignKey, primaryKey } from 'drizzle-orm/pg-core';
+import { usersTable } from '@/models/user.model';
+
+export const packagesTable = pgTable(
+    'packages',
+    {
+        id: serial('id').primaryKey(),
+        userId: integer('user_id')
+            .notNull()
+            .references(() => usersTable.userId, { onDelete: 'cascade' }),
+        title: varchar('title', { length: 255 }).notNull(),
+        parentId: integer('parent_id'),
+        createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
+    },
+    table => {
+        return {
+            parentFk: foreignKey({
+                columns: [table.parentId],
+                foreignColumns: [table.id],
+                name: 'packages_parent_id_fkey',
+            }).onDelete('set null'),
+        };
+    }
+);
--- src/models/topic/topic.model.ts
+++ src/models/topic/topic.model.ts
@@ -1,12 +1,16 @@
 import { pgTable, serial, integer, varchar, text, timestamp, vector } from 'drizzle-orm/pg-core';
 import { usersTable } from '@/models/user.model'; // adjust path as needed
 import { classesTable } from '@/models/class-based-learning/class.model';
+import { packagesTable } from '../package/package.model';
 
 export const topicsTable = pgTable('topics', {
     topicId: serial('topic_id').primaryKey(),
+    // package id can be NULL
+    package_id: integer('package_id').references(() => packagesTable.id, {
+        onDelete: 'set null',
+    }),
     // topic belong to which class, classId can be NULL if that topic belong to a user (student) with no class-based learning
-    classId: integer('class_id')
-        .references(() => classesTable.classId, { onDelete: 'cascade' }),
+    classId: integer('class_id').references(() => classesTable.classId, { onDelete: 'cascade' }),
     userId: integer('user_id')
         .notNull()
         .references(() => usersTable.userId, { onDelete: 'cascade' }),
