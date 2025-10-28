167e0c6a1985d75ffe5f5567fbd18d5c392a3c7e
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2025-10-28 15:35:03 +0700

    fix(learningMaterial): allow images real

--- src/services/uploads/files/upload.file.R2.service.ts
+++ src/services/uploads/files/upload.file.R2.service.ts
@@ -101,12 +101,12 @@ export class UploadFileService {
                 'text/html',
                 'text/xml',
                 // Images
-                // 'image/jpeg',
-                // 'image/png',
-                // 'image/gif',
-                // 'image/bmp',
-                // 'image/webp',
-                // 'image/svg+xml',
+                'image/jpeg',
+                'image/png',
+                'image/gif',
+                'image/bmp',
+                'image/webp',
+                'image/svg+xml',
                 // Archives
                 'application/zip',
                 'application/x-rar-compressed',
@@ -115,7 +115,7 @@ export class UploadFileService {
                 'application/gzip',
             ],
             uploadDir: config?.uploadDir || generateConfig.uploadDir,
-            allowedExtensions: config?.allowedExtensions || ['.pdf', '.doc', '.docx', '.txt', '.md'],
+            allowedExtensions: config?.allowedExtensions || ['.pdf', '.doc', '.docx', '.txt', '.md', '.jpeg', '.png'],
         };
     }
 
