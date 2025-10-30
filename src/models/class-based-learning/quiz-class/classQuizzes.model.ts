eeecefdeb255f0bec710d046d39361873abfddf9
thai-hoang-03 <buithaihoang03@gmail.com>
2025-10-30 23:48:40 +0700

    chore(quiz-class): drop quizID

--- src/models/class-based-learning/quiz-class/classQuizzes.model.ts
+++ src/models/class-based-learning/quiz-class/classQuizzes.model.ts
@@ -1,25 +1,25 @@
 import { pgTable, serial, integer, text, timestamp, boolean, varchar } from "drizzle-orm/pg-core";
 import { usersTable, classesTable, topicsTable } from "@/models";
-import { quizzesTable } from "@/models/quiz/quiz.model";
 import { pgEnum } from "drizzle-orm/pg-core";
 
 export const classQuizStatusEnum = pgEnum('class_quiz_status', ['draft','scheduled','published','closed']);
 
 export const classQuizzesTable = pgTable('class_quizzes', {
   classQuizId: serial('class_quiz_id').primaryKey(),
+
   teacherId: integer('teacher_id').notNull().references(() => usersTable.userId, { onDelete: 'cascade' }),
   classId: integer('class_id').notNull().references(() => classesTable.classId, { onDelete: 'cascade' }),
   topicId: integer('topic_id').references(() => topicsTable.topicId, { onDelete: 'cascade' }), // nullable
+
   title: varchar('title', { length: 255 }).notNull(),
   content: text('content').notNull().default(''),
-  quizId: integer('quiz_id').notNull().references(() => quizzesTable.quizId, { onDelete: 'cascade' }),
 
-  //Hybrid
-  startAt: timestamp('start_at', { withTimezone: true }),   
-  endAt: timestamp('end_at', { withTimezone: true }),       
-  durationSeconds: integer('duration_seconds'),             
+  // Hybrid window
+  startAt: timestamp('start_at', { withTimezone: true }),
+  endAt: timestamp('end_at', { withTimezone: true }),
+  durationSeconds: integer('duration_seconds'),
 
-  // rule & display
+  // Rules
   maxAttempts: integer('max_attempts').notNull().default(1),
   shuffleQuestions: boolean('shuffle_questions').notNull().default(true),
   shuffleChoices: boolean('shuffle_choices').notNull().default(true),
