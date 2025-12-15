0fd545347dfdcd81655b7f84a9e788643e664a09
KyAnh1126 <anh261104@gmail.com>
2025-12-15 15:54:54 +0700

    feat(media-transcription): rename route for media transcription

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -53,7 +53,7 @@ import './comment/comment.routes';
 import './content/content.routes';
 import './embedding/embedding.route';
 import './image/image.routes';
-import './audioTranscription/audioTranscription.routes';
+import './mediaTranscription/mediaTranscription.routes';
 import './convert/convert.routes';
 
 // Apply global async handler to router
